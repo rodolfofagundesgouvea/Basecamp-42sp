f02aa38152b6ef1794673015bd3f70c3f06b3020
rfagunde <rfagunde@student.42sp.org.br>
2021-04-08 18:53:48 +0000

    Commit do ex05

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfagunde <rfagunde@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/08 00:16:47 by rfagunde          #+#    #+#             */
+/*   Updated: 2021/04/08 18:52:35 by rfagunde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char x, char y, char z)
+{
+	write(1, &x, 1);
+	write(1, &y, 1);
+	write(1, &z, 1);
+	if (x != '7' || y != '8' || z != '9')
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char x;
+	char y;
+	char z;
+
+	x = '0';
+	while (x <= '7')
+	{
+		y = x + 1;
+		while (y <= '8')
+		{
+			z = y + 1;
+			while (z <= '9')
+			{
+				ft_putchar(x, y, z);
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+}
