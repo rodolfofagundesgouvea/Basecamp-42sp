13a461f9c830a36ea7deb297a5b0496d98906baa
rfagunde <rfagunde@student.42sp.org.br>
2021-04-15 20:48:54 +0000

    Commit do ex00

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfagunde <rfagunde@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 16:53:28 by rfagunde          #+#    #+#             */
+/*   Updated: 2021/04/15 17:05:06 by rfagunde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int count;
+
+	count = 0;
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
